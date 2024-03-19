b3d9192461c52d3138f2995d5e51ed825efdd078
Nelson Mecha <nelsonorinamecha@gmail.com>
2024-03-19 10:26:07 +0300

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(scooterRide){
+  if (scooterRide <= 400){
+    return "This one is on me!"
+  }else if(scooterRide <= 2000) {
+    return "That will be twenty bucks."
+  }else if(scooterRide > 2000 && scooterRide <= 2500){
+    return "I will gladly take your thirty bucks."
+  }else if(scooterRide > 2500){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'    
+  }
+
+  
+
 }
